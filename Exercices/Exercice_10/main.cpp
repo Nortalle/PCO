8f16de9a5b175df9ed42759b69949c5eb822c89e
Nortalle <vincent.guidoux1@heig-vd.ch>
2018-06-27 13:59:18 +0200

    Exercice 10 commencé

--- Exercices/Exercice_10/main.cpp
+++ Exercices/Exercice_10/main.cpp
@@ -1,8 +1,40 @@
-#include <QCoreApplication>
+
+#include <QThread>
+
+#include "pcomuter_qt.h"
+
+
+
+#include <QThread>
+#include <iostream>
+
+
+static int global = 0;
+static int iterations = 1000000;
+static PcoMutex mutex;
+
+class CounterThread: public QThread
+{
+public:
+     void run() Q_DECL_OVERRIDE {
+        for(int i=0;i<iterations;i++) {
+            mutex.lock();
+            global = global + 1;
+            mutex.unlock();
+        }
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    CounterThread threads[2];
+    for(int i=0;i<2;i++)
+        threads[i].start();
+    for(int i=0;i<2;i++)
+        threads[i].wait();
 
-    return a.exec();
+    std::cout << "Fin des taches : global = " << global
+              << " (" << 2*iterations << ")" << std::endl;
+
+    return 0;
 }
---
+++ Exercices/Exercice_10/pcomuter_qt.h
@@ -0,0 +1,70 @@
+#ifndef PCOMUTER_QT_H
+#define PCOMUTER_QT_H
+#include <QSemaphore>
+#include <iostream>
+
+
+class PcoMutex
+{
+    QSemaphore mutex;
+    QSemaphore attente;
+
+    int nbThreadWaiting;
+    bool occupied;
+public:
+    PcoMutex() : occupied(false), nbThreadWaiting(0), mutex(1), attente(0)
+    {
+
+    }
+
+    ~PcoMutex()
+    {
+
+    }
+
+    void lock()
+    {
+        mutex.acquire();
+
+        if(occupied){
+
+            nbThreadWaiting ++;
+            attente.acquire();
+        }else{
+            occupied = true;
+        }
+        mutex.release();
+    }
+
+    void unlock()
+    {
+        mutex.acquire();
+        if(occupied){
+            if (nbThreadWaiting>0) {
+                nbThreadWaiting--;
+                attente.release();
+            }
+            else {
+                occupied = false;
+            }
+        }
+        mutex.release();
+    }
+
+    bool trylock()
+    {
+        mutex.acquire();
+        if (occupied) {
+            mutex.release();
+            return false;
+        }
+        else {
+            occupied = true;
+            mutex.release();
+            return true;
+        }
+    }  //! Returns true if the mutex can be acquired, false if it is already locked
+};
+
+
+#endif // PCOMUTER_QT_H
